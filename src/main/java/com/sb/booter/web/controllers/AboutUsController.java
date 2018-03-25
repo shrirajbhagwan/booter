e59d96c96ac85be8de9c7936929f90e3bcd71a07
shrirajbhagwan <bhagwan.shriraj@gmail.com>
2018-03-25 12:46:29 +0530

    aboutus page added

---
+++ src/main/java/com/sb/booter/web/controllers/AboutUsController.java
@@ -0,0 +1,15 @@
+package com.sb.booter.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutUsController {
+
+	@RequestMapping("/about")
+	public String about(){
+		
+		return "about";
+	}
+	
+}
